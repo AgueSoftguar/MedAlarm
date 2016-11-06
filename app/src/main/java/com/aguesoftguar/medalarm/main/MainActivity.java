9c008435c6972cf95b42ed27290f55593141dda0
Francisco Javier Martin Otero <fjavier.mo@gmail.com>
2016-11-06 11:47:24 +0100

    Resolve #10 - Create base clean architecture. Include libs: guava & support-annotations.
      https://github.com/google/guava/wiki
      https://developer.android.com/reference/android/support/annotation/package-summary.html

--- app/src/main/java/com/aguesoftguar/medalarm/main/MainActivity.java
+++ app/src/main/java/com/aguesoftguar/medalarm/main/MainActivity.java
@@ -1,13 +1,32 @@
-package com.aguesoftguar.medalarm;
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aguesoftguar.medalarm.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.aguesoftguar.medalarm.ActivityUtils;
+import com.aguesoftguar.medalarm.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        MainFragment tasksFragment =
+                (MainFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null) {
+            // Create the fragment
+            tasksFragment = MainFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
     }
 
     @Override
