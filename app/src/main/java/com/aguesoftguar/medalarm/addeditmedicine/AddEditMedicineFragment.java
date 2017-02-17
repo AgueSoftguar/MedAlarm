ca58c298b867bd9555531d97de8fbf1ef69b591e
Sara Lucía Pérez <babelia13@gmail.com>
2017-02-17 22:16:52 +0100

    Resolve #26 - Add dialogs to create a new patient and select a patient for a medicine

--- app/src/main/java/com/aguesoftguar/medalarm/addeditmedicine/AddEditMedicineFragment.java
+++ app/src/main/java/com/aguesoftguar/medalarm/addeditmedicine/AddEditMedicineFragment.java
@@ -16,28 +16,37 @@
 
 package com.aguesoftguar.medalarm.addeditmedicine;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.aguesoftguar.medalarm.R;
+import com.aguesoftguar.medalarm.data.Patient;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -65,6 +74,9 @@ public class AddEditMedicineFragment extends Fragment implements AddEditMedicine
    @BindView(R.id.select_notification_radiobutton) RadioButton selectNotificationRadioButton;
    @BindView(R.id.select_color_radiobutton) RadioButton selectColorRadioButton;
    @BindView(R.id.select_image_radiobutton) RadioButton selectImageRadioButton;
+
+   // TODO 17/02/2017 Remove this list and get the patients from the repository
+   private ArrayList<Patient> patients = new ArrayList<>();
 
    private AddEditMedicineContract.Presenter presenter;
 
@@ -98,6 +110,17 @@ public class AddEditMedicineFragment extends Fragment implements AddEditMedicine
                                       Bundle savedInstanceState) {
       View rootView = inflater.inflate(R.layout.fragment_add_and_edit_medicine, container, false);
       ButterKnife.bind(this, rootView);
+
+      // TODO 17/02/2017 Remove the following lines when the repository is implemented
+      patients.add(new Patient("Sara", null));
+      patients.add(new Patient("Fran", null));
+
+      // Show a dialog to select the patient when clicking the proper text view
+      addPatientTextView.setOnClickListener(new View.OnClickListener() {
+         @Override public void onClick(View v) {
+            showSelectPatientDialog();
+         }
+      });
 
       // Get the current date
       Date date = new Date();
@@ -213,6 +236,92 @@ public class AddEditMedicineFragment extends Fragment implements AddEditMedicine
       return rootView;
    }
 
+   /**
+    * Create a custom {@link AlertDialog} to select a patient and show it.
+    */
+   public void showSelectPatientDialog() {
+
+      LayoutInflater inflater = LayoutInflater.from(AddEditMedicineFragment.this.getContext());
+      View contentView = inflater.inflate(R.layout.dialog_add_patient, null);
+      View footerView = inflater.inflate(R.layout.dialog_add_patient_footer, null);
+
+      final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(AddEditMedicineFragment.this.getContext());
+      dialogBuilder.setTitle(getString(R.string.add_patient_title));
+      dialogBuilder.setView(contentView);
+
+      final AlertDialog alertDialog = dialogBuilder.create();
+
+      ListView patientsList = (ListView) contentView.findViewById(R.id.patients_listview);
+      patientsList.addFooterView(footerView);
+      PatientsAdapter patientsAdapter = new PatientsAdapter(patients, new PatientsAdapter.PatientItemListener() {
+         @Override public void onPatientClick(Patient clickedPatient) {
+            addPatientTextView.setText(clickedPatient.getName());
+            alertDialog.dismiss();
+         }
+      });
+      patientsList.setAdapter(patientsAdapter);
+
+      footerView.setOnClickListener(new View.OnClickListener() {
+         @Override public void onClick(View v) {
+            showAddNewPatientDialog();
+            alertDialog.dismiss();
+         }
+      });
+
+      alertDialog.show();
+   }
+
+   /**
+    * Create a custom {@link AlertDialog} to add a new patient and show it.
+    */
+   public void showAddNewPatientDialog() {
+      LayoutInflater inflater = LayoutInflater.from(AddEditMedicineFragment.this.getContext());
+      View contentView = inflater.inflate(R.layout.dialog_add_new_patient, null);
+      final TextInputEditText patientNameEditText = (TextInputEditText) contentView.findViewById(R.id.new_patient_name_edittext);
+      final TextInputLayout patientNameInputLayout = (TextInputLayout) contentView.findViewById(R.id.new_patient_name_inputlayout);
+
+      AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(AddEditMedicineFragment.this.getContext());
+      dialogBuilder.setTitle(getString(R.string.add_patient_title));
+      dialogBuilder.setView(contentView);
+      dialogBuilder.setPositiveButton(R.string.action_accept, null);
+      dialogBuilder.setNegativeButton(R.string.action_cancel, null);
+
+      final AlertDialog alertDialog = dialogBuilder.create();
+      alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+         @Override
+         public void onShow(final DialogInterface dialog) {
+
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+
+               @Override
+               public void onClick(View view) {
+                  if (TextUtils.isEmpty(patientNameEditText.getText())) {
+                     patientNameInputLayout.setErrorEnabled(true);
+                     patientNameInputLayout.setError(getResources().getString(R.string.add_patient_name_empty_error));
+                  } else {
+                     patientNameInputLayout.setErrorEnabled(false);
+                     patients.add(new Patient(patientNameEditText.getText().toString(), null));
+                     addPatientTextView.setText(patientNameEditText.getText().toString());
+                     dialog.dismiss();
+                  }
+               }
+            });
+
+            alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+
+               @Override
+               public void onClick(View view) {
+                  dialog.dismiss();
+               }
+            });
+         }
+      });
+
+      alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+      alertDialog.show();
+   }
+
    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
       inflater.inflate(R.menu.add_and_edit_medicine_settings, menu);
    }
@@ -220,5 +329,4 @@ public class AddEditMedicineFragment extends Fragment implements AddEditMedicine
    @Override public boolean isActive() {
       return isAdded();
    }
-
 }
---
+++ app/src/main/java/com/aguesoftguar/medalarm/addeditmedicine/PatientsAdapter.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2016 Agüesoftgüar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aguesoftguar.medalarm.addeditmedicine;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.aguesoftguar.medalarm.R;
+import com.aguesoftguar.medalarm.data.Patient;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * {@link BaseAdapter} used in the dialog used to show the patients list when
+ * adding a new medicine.
+ */
+public class PatientsAdapter extends BaseAdapter {
+
+   private List<Patient> patients;
+   private PatientItemListener itemListener;
+
+   public PatientsAdapter(List<Patient> patients, PatientItemListener patientItemListener) {
+      setList(patients);
+      itemListener = patientItemListener;
+   }
+
+   /**
+    * Replace the current data by a new {@link Patient} list.
+    *
+    * @param patients New list of {@link Patient}.
+    */
+   public void replaceData(List<Patient> patients) {
+      setList(patients);
+      notifyDataSetChanged();
+   }
+
+   private void setList(List<Patient> tasks) {
+      patients = checkNotNull(tasks);
+   }
+
+   @Override
+   public int getCount() {
+      return patients.size();
+   }
+
+   @Override
+   public Patient getItem(int i) {
+      return patients.get(i);
+   }
+
+   @Override
+   public long getItemId(int i) {
+      return i;
+   }
+
+   @Override
+   public View getView(int i, View view, ViewGroup viewGroup) {
+      View rowView = view;
+      if (rowView == null) {
+         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+         rowView = inflater.inflate(R.layout.dialog_add_patient_row, viewGroup, false);
+      }
+
+      final Patient patient = getItem(i);
+
+      TextView patientNameTextView = (TextView) rowView.findViewById(R.id.patient_name);
+      patientNameTextView.setText(patient.getName());
+
+      ImageView patientImageView = (ImageView) rowView.findViewById(R.id.patient_image);
+      if (patient.getPhoto() != null) {
+         //TODO 17/02/2017 Set the patient image
+      }
+
+      rowView.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            itemListener.onPatientClick(patient);
+         }
+      });
+
+      return rowView;
+   }
+
+   /**
+    * Callback used to communicate with the class that handle the ListView.
+    */
+   public interface PatientItemListener {
+
+      /**
+       * Called when a patient of the list has been selected.
+       *
+       * @param clickedPatient {@link Patient} object related with the selected position.
+       */
+      void onPatientClick(Patient clickedPatient);
+   }
+}
